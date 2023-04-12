fe2b5446fcd78978e14022c0ab4cbb7f57944a27
Babjidurga <Babji2662@gamil.com>
2023-04-12 16:32:03 +0530

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+      count: 0
+    }
+  }
+
+  delHeader = () => {
+    this.setState({ show: false });
+  }
+
+  componentDidMount() {
+    console.log("This will be called immediately after the return of render")
+  }
+
+  componentDidUpdate() {
+    console.log("This will be called after every update")
+  }
+
+  render() {
+    let myHeader;
+    if (this.state.show) {
+      myHeader = <Child />
+    }
+    return (
+      <div>
+        <center>
+          {myHeader}
+          <button type='button' onClick={this.delHeader}>Delete Header</button>
+          <hr />
+          <hr />
+          <h3>count: {this.state.count}</h3>
+          <button type='button' onClick={() => this.setState({ count: this.state.count + 1 })}>Increase</button>
+          <button type='button' onClick={() => this.setState({ count: this.state.count - 1 })}>Decrease</button>
+          <hr />
+          <hr />
+        </center>
+      </div>
+    );
+  }
 }
 
-export default App;
+class Child extends React.Component {
+  componentWillUnmount() {
+    console.log("This method will be called if any things get deleted like items")
+  }
+
+  render() {
+    return (
+      <h1>Hello this is the string</h1>
+    )
+  }
+}
+
+export default Container;
