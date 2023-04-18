61c452acbf0b875a66d858b476b836436cffb6ce
Babjidurga <Babji2662@gmail.com>
2023-04-18 10:37:09 +0530

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,53 @@
-import React from 'react'
+// In simple js file it will not give any output but in React js file it will give the output
 
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: true,
-      count: 0
-    }
-  }
+import React,{useState} from 'react'
+import emailjs from 'emailjs-com'
+import './App.css'
 
-  delHeader = () => {
-    this.setState({ show: false });
+function App(){
+  const [data,setData] = useState({
+    subject:"",
+    from_name:"",
+    email:"",
+    message:"",
+    phone:"",
+  })
+  const {subject,from_name, email,message,phone} = data;
+  const onChange = e => {
+    setData({...data,[e.target.name]:[e.target.value]})
   }
+  const submitHandler = e => {
+    e.preventDefault();
+    emailjs.sendForm('service_k8par4z', 'template_thq985s', e.target , '3ZEwERPJS7LwnAFCV')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  }
+  return (
+    <div>
+      <center>
+        <h3 className='first-head'> QUERY SUBMISSION FORM</h3>
+        <form onSubmit={submitHandler}>
+        <label for="from_name"> Name: </label> 
+          <input type = "text" name="from_name" value={from_name} onChange={onChange} placeholder='Your good name' required/>
+          <label for="subject"> Subject: </label>
+          <input type = "text" name="subject" value={subject} onChange={onChange} placeholder='Subject' required/>
+          <label for="email"> Email: </label>
+          <input type = "email" name="email" value={email} onChange={onChange} placeholder='Email' required/>
 
-  componentDidMount() {
-    console.log("This will be called immediately after the return of render")
-  }
+          <label for="message"> Message: </label>
+          <input type = "text" name="message" value={message} onChange={onChange} placeholder='Your Message' required/>
 
-  componentDidUpdate() {
-    console.log("This will be called after every update")
-  }
-
-  render() {
-    let myHeader;
-    if (this.state.show) {
-      myHeader = <Child />
-    }
-    return (
-      <div>
-        <center>
-          {myHeader}
-          <button type='button' onClick={this.delHeader}>Delete Header</button>
-          <hr />
-          <hr />
-          <h3>count: {this.state.count}</h3>
-          <button type='button' onClick={() => this.setState({ count: this.state.count + 1 })}>Increase</button>
-          <button type='button' onClick={() => this.setState({ count: this.state.count - 1 })}>Decrease</button>
-          <hr />
-          <hr />
-        </center>
-      </div>
-    );
-  }
+          <label for="phone"> Mobile: </label>
+          <input type = "number" name="phone" value={phone} onChange={onChange} placeholder='Phone number' required/>
+          <input type="submit" name="submit" className='button'/>
+        </form>
+      </center>
+    </div>
+  )
 }
 
-class Child extends React.Component {
-  componentWillUnmount() {
-    console.log("This method will be called if any things get deleted like items")
-  }
-
-  render() {
-    return (
-      <h1>Hello this is the string</h1>
-    )
-  }
-}
-
-export default Container;
+export default App
